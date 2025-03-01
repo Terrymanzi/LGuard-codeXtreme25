cef00afc7d6d902ebba323e51ca5efc87b93fe0d
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-03-01 09:56:14 +0200

    WiFi & Firebase Setup → Configuring network and Firebase

--- hardware/Lgaurd.cpp
+++ hardware/Lgaurd.cpp
@@ -2,6 +2,18 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
+#include <ESP8266WiFi.h>
+#include <Firebase_ESP_Client.h>
+
+
+// **Network Credentials**
+#define WIFI_SSID "Levenn"
+#define WIFI_PASSWORD "11223344"
+
+// **Firebase Credentials**
+#define API_KEY "AIzaSyD4ZX5wKS94-DktiJBfZeziNdz447Tb7kw"
+#define DATABASE_URL "https://l-guard-default-rtdb.europe-west1.firebasedatabase.app/"
+
 
 // **Ultrasonic Sensor Pins**
 #define TRIG_PIN D6
@@ -15,6 +27,35 @@
 
 // **ADXL345 Accelerometer**
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
+// **Firebase Objects**
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println("\nConnected to Wi-Fi");
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop() {
+  Serial.println("WiFi & Firebase connected...");
+  delay(2000);
+}
+
 
 void setup() {
   Serial.begin(115200);
